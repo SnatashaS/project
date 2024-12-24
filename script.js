d4195d132bd3e727d7615d8291ec34988addae69
Natasha Serdukova <natashaserdukova@gmail.com>
2024-12-24 18:04:00 +0300

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+$('nav ul li:nth-child(1)').click(function () {
+    $('nav ul li:nth-child(1)').css("background-color", "#6AB2B1")
+    $('nav ul li:nth-child(1)').css("padding", "10px")
+})
+
+$('nav ul li:nth-child(2)').click(function () {
+    $('nav ul li:nth-child(2)').css("background-color", "#6AB2B1")
+    $('nav ul li:nth-child(2)').css("padding", "10px")
+})
+
+$('nav ul li:nth-child(3)').click(function () {
+    $('nav ul li:nth-child(3)').css("background-color", "#6AB2B1")
+    $('nav ul li:nth-child(3)').css("padding", "10px")
+})
+
+$('nav ul li:nth-child(4)').click(function () {
+    $('nav ul li:nth-child(4)').css("background-color", "#6AB2B1")
+    $('nav ul li:nth-child(4)').css("padding", "10px")
+})
